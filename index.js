c28965efd0e617c61caca1a8f78334f29fe34c07
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-04-18 19:57:20 +0200

    idk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function reply_click(clicked_id) {
+    document.getElementById("titles").style.display = "flex";
     // Hide cards
     let elements = Array.from(document.getElementsByClassName("optionCard")); // Convert to array
     elements.forEach(element => {
@@ -10,7 +11,7 @@ function reply_click(clicked_id) {
             console.log(element.id + " conserved");
         }
     });
-
+    
     // Show a card randomly
 
     let remainingElements = Array.from(document.getElementsByClassName("optionCard"));
@@ -25,41 +26,26 @@ function reply_click(clicked_id) {
         randomElement.style.display = "flex"; 
         console.log("hay 2 iguales");
         
-        if (clicked_id == randomElement.id) {
+        if (clicked_id === randomElement.id) {
             const originalCard = document.getElementById(randomElement.id);
             const clonedCard = originalCard.cloneNode(true);
-            //let positionCard = originalCard.parentNode.appendChild(clonedCard);
             
-            clonedCard.id = "otherCard";
             
+            clonedCard.id = clicked_id;
+            console.log("esta es la carta que usara el contrincante: " + clonedCard.id);
            console.log("hay 2 iguales");
            
-        } else {
-            
+        } else {  
             console.log("no hay 2 iguales");
         }
-    
-           
         
     } else {
         console.log("There are no remaining items to display.");
     }
+    document.getElementById("otherCard").style.display = "flex";
     
-    switch (randomElement.id){
-        case "paper":
-            document.getElementById("otherCard").style.backgroundImage = "url('images/icon-paper.svg')";
-            console.log("PAPEL");
-            break;
-        case "scissors":
-            document.getElementById("otherCard").style.backgroundImage = "url('images/icon-scissors.svg')";
-            console.log("TIJERAS");
-            break;
-        case "rock":
-            document.getElementById("otherCard").style.backgroundImage = "url('images/icon-rock.svg')";
-            console.log("PIEDRA");
-            break;
-       }
-    
-    document.getElementById("titles").removeAttribute("hidden");
-
+    clicked_id.id = "yourCard";
+    console.log("ahora es"+clicked_id);
+      console.log("esto es lo ultimo");
+       document.getElementById("yourCard").removeAttribute("hidden");
 }
